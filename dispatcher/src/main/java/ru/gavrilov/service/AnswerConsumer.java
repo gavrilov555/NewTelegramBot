8b3254c2671857d404551a3e620f2f8d5c137862
gavrilov555 <gavrilov_v92@mail.ru>
2022-12-17 15:44:14 +0500

    Initial commit

---
+++ dispatcher/src/main/java/ru/gavrilov/service/AnswerConsumer.java
@@ -0,0 +1,7 @@
+package ru.gavrilov.service;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public interface AnswerConsumer {
+    void consumer (SendMessage sendMessage);
+}
