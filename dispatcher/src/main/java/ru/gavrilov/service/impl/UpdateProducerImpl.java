839f5c2248501430e92fcb7a09191fddcebc92aa
gavrilov555 <gavrilov_v92@mail.ru>
2022-12-19 12:56:06 +0500

    add rabbitmq, docker

--- dispatcher/src/main/java/ru/gavrilov/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ru/gavrilov/service/impl/UpdateProducerImpl.java
@@ -1,6 +1,7 @@
 package ru.gavrilov.service.impl;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.gavrilov.service.UpdateProducer;
@@ -8,9 +9,17 @@ import ru.gavrilov.service.UpdateProducer;
 @Service
 @Log4j
 public class UpdateProducerImpl implements UpdateProducer {
+
+    private final RabbitTemplate rabbitTeamplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTeamplate) {
+        this.rabbitTeamplate = rabbitTeamplate;
+    }
+
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
-
+        rabbitTeamplate.convertAndSend(rabbitQueue,update);
     }
 }
